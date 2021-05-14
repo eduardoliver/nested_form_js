c3e31243e92529bd0b82e14dfa4d2c277e4bcd24
Eduard Oliver <eduardoliver@gmail.com>
2021-05-14 22:50:37 +0100

    yarn add sortablejs

--- app/javascript/packs/controllers/drag_controller.js
+++ app/javascript/packs/controllers/drag_controller.js
@@ -2,7 +2,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "links", "template" ]
+  static targets = []
 
   initialize(){
    // console.log ("init");
@@ -10,27 +10,5 @@ export default class extends Controller {
 
   connect() {
    // console.log ("connect")
-  }
-
-  add_association(event) {
-    // console.log("Click add task")
-    event.preventDefault()
-
-    var content = this.templateTarget.innerHTML.replace(/NEW_RECORD/g, (new Date).getTime())
-    this.linksTarget.insertAdjacentHTML("beforebegin", content)
-  }
-
-  remove_association(event) {
-    event.preventDefault()
-
-    let wrapper = event.target.closest(".nested-fields")
-    // console.log ("remove_association", wrapper.dataset.newRecord)
-    if (wrapper.dataset.newRecord == "true") {
-      wrapper.remove()
-    }
-    else {
-      wrapper.querySelector("input[name*='_destroy']").value = 1
-      wrapper.style.display = 'none'
-    }
   }
 }
